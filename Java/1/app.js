ca498bb6dcc0d5382f13250e0c911d8163b2078b
mshahzaib <mshahzaib101ed@gmail.com>
2017-03-28 23:13:11 +0500

    practice

---
+++ Java/1/app.js
@@ -0,0 +1,6 @@
+var num = 5;
+var num2 = 3;
+var num3 = num + num2 - num3;
+alert(num3);
+
+
---
+++ Java/2/app.js
@@ -0,0 +1,217 @@
+// // // // // // // // // var name = prompt("What is your Name");
+// // // // // // // // // var welcome = alert("Welcome to your Account");
+// // // // // // // // // var error = prompt("Plese enter your name");
+// // // // // // // // // if (name === "shahzaib")
+// // // // // // // // // {msg = welcome}
+// // // // // // // // // else if (error === "shahzaib")
+// // // // // // // // // {msg = welcome}
+// // // // // // // // // else{msg = error}
+
+// // // // // // // // -------------------------------------------------------------------------------------
+
+// // // // // // // // // Assignment 1,
+// // // // // // // // // 1. Write a program that takes input a name from user & greet the
+// // // // // // // // // user like this:
+// // // // // // // // // var name = prompt("Please enter your name");
+// // // // // // // // // var ans = "M Shahzaib Shoaib";
+// // // // // // // // // if (name === "shahzaib")
+// // // // // // // // // {
+// // // // // // // // //     alert("Hi, " + ans);
+// // // // // // // // // }
+
+// // // // // // // // -----------------------------------------------------------------------------------------
+
+// // // // // // // // // Assignment 2,
+// // // // // // // // // 2. Write a program to take input a number from user & display
+// // // // // // // // // it’s multiplication table on your browser. If user does not enter
+// // // // // // // // // a new number, multiplication table of 5 should be displayed by
+// // // // // // // // // default.
+
+
+
+// // // var num = prompt("Enter a number to print its multiplication table.");
+// // // var unit = 0;
+// // // document.write("Multiplicatio table of  " + num + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // document.write(num + " X " + ++unit + " = " + num * unit + "</br>")
+// // // // --------------------------------------------------------------------------------------------
+
+
+// // // // // // // // Assignment 3,
+// // // // // // // // // 3. Write a program to take “city” name as input from user. If user
+// // // // // // // // // enters “Karachi”, welcome the user like this: “Welcome to city
+// // // // // // // // // of lights”
+
+
+// // // // // // // // // var cityName = prompt("Please enter your city");
+// // // // // // // // // if(cityName === "karachi")
+// // // // // // // // // {
+// // // // // // // // //     alert("Welcome to city of lights")
+// // // // // // // // // }
+
+// // // // // // // -----------------------------------------------------------------------------------------------------
+
+// // // // // // // Assignment 4,
+// // // // // // // 4. Write a program to take “gender” as input from user. If the
+// // // // // // // user is male, give the message: Good Morning Sir. If the user is
+// // // // // // // female, give the message: Good Morning Ma’am.
+
+// // // // // // // var gender = prompt("Please enter your gender ");
+// // // // // // // if(gender === "male")
+// // // // // // // {alert("Good Morning Sir")}
+// // // // // // // else if(gender === "female")
+// // // // // // // {alert("Good Morning Ma’am")}
+
+// // // // // // // --------------------------------------------------------------------------------------------------------------
+
+// // // // // // // Assignment 5,
+// // // // // // // 5. Write a program to take input color of road traffic signal from the user & show the message according to this table: 
+
+
+// // // // // // var color = prompt("Enter any color of road traffic signal");
+// // // // // // if (color === "red") {
+// // // // // //     document.write("<h1>vehicles must stop </h1>");
+// // // // // // }
+// // // // // // else if (color === "yellow") {
+// // // // // //     document.write("<h1>vehicles should get ready to move </h1>");
+// // // // // // }
+// // // // // // else if(color === "green") {
+// // // // // //     document.write("<h1>vehicles can move now  </h1>");
+// // // // // // }
+// // // // // // else {
+// // // // // //     document.write("<h1>This is not a Trafic Signal color.</h1>");
+// // // // // // }
+
+// // // // // // -------------------------------------------------------------------------------------------------------------------------
+
+// // // // // // // Assignment 6,
+// // // // // //6.  Write a program to take input max age & current age from user. If the current age is less than or equal to max age, show 
+// // // // // //the message “You are welcome” 
+
+// // // // // var currentAge = prompt("Enter your Current age.");
+// // // // // var maxAge = prompt("Enter your Max age.");
+// // // // // if (currentAge <= maxAge) {
+// // // // //     alert("You are welcome");
+// // // // // }
+
+// // // // // ----------------------------------------------------------------------------------------------------------------------------
+
+// // // //  // Assignment 7,
+// // // //  // 7. Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car” 
+
+// // // //  var fuel = prompt("Enter the current remaining fuel(in liters).")
+// // // //  if (fuel <= 0.25) {
+// // // //      alert("Refill your tank.");
+// // // //  }
+// // // //  else {
+// // // //      alert("continue driving");
+// // // //  }
+
+// // // //  ----------------------------------------------------------------------------------------------------------------------------------
+
+
+// // // Assignment 8,
+// // // var a = 4; if (++a === 5){     alert("given condition for variable a is true"); }  
+// //  //var b = 82; if (b++ === 82){     alert("given condition for variable b is true"); }  
+// //  // var c = 12; if (c++ === 13){     alert("condition 1 is true"); } if (c === 13){     alert("condition 2 is true"); } if (++c < 14){     alert("condition 3 is true"); } if(c === 14){     alert("condition 4 is true"); }  
+// //  //var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost; if (totalCost === laborCost + materialCost){     alert("The cost equals"); }  
+// // //if (true){     alert("True"); } if (false){     alert("False"); }  
+// // //if("car" < "cat"){     alert("car is smaller than cat"); } 
+
+// // // --------------------------------------------------------------------------------------------------------------------------------
+
+
+// // // Assignment 9,
+// // // 9. Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table:  
+
+
+// // var math = +prompt("Enter your maths obtained marks(out of hundred).");
+// // var english = +prompt("Enter your english obtained marks(out of hundred).");
+// // var computer = +prompt("Enter your computer obtained marks(out of hundred).");
+// // var obtainMarks = math + english + computer;
+// // var percentage = (obtainMarks / 300) * 100;
+// // document.write("<h1>Mark Sheet</h1>",
+// // "Total Marks : 300",
+// // "</br>Marks Obtain : " + obtainMarks,
+// // "</br>Percentage : " + percentage);
+// // //grade
+// // if (percentage >= 80) {
+// //     document.write("</br>Grade : A-one","</br>Remarks : Excellent");
+// // }
+// // else if (percentage >= 70) {
+// //     document.write("</br>Grade : A","</br>Remarks : Good ");
+// // }
+// // else if (percentage >= 60) {
+// //     document.write("</br>Grade : B","</br>Remarks : You need to improve ");
+// // }
+// // else if (percentage < 60) {
+// //     document.write("</br>Grade : Fail","</br>Remarks : Sorry");
+// // }
+
+// // ---------------------------------------------------------------------------------------------------------------
+
+// // Assignment 10,
+// //10. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Take input from users, the following:  a. Name of item1  b. Name of item2 c. Price of item 1 d. Price of item 2 e. Ordered quantity of item 1  f. Ordered Quantity of item 2 g. Shipping charges Compute the total cost. If the total cost is above 2000 PKR , offer them 10% discount & show the receipt in your browser. 
+
+// // var itemOneName = prompt("Enter your first purchased item.");
+// // var itemOnePrice = prompt("Enter price of " + itemOneName);
+// // var itemOneQuantity = prompt("Enter quantity of " + itemOneName + " ,you bought");
+// // var itemTwoName = prompt("Enter your second purchased item.");
+// // var itemTwoPrice = prompt("Enter price of " + itemTwoName);
+// // var itemTwoQuantity = prompt("Enter quantity of " + itemTwoName + " ,you bought");
+// // var shipping = prompt("Shipping Charges?");
+// // var total = (itemOnePrice * itemOneQuantity) + (itemTwoPrice * itemTwoQuantity) + shipping;
+// // document.write(
+// //     "<h1>Shopping Cart</h1></br>",
+// //     "Price of " + itemOneName + " is " + itemOnePrice,
+// //     "<br />Quantity of " + itemOneName + " is " + itemOneQuantity,
+// //     "<br />Price of " + itemTwoName + " is " + itemTwoPrice,
+// //     "<br />Quantity of " + itemTwoName + " is " + itemTwoQuantity,
+// //     "<br /></br>Shipping charges " + shipping,
+// //     "<br />Total Cost of your order is " + total)
+
+// // -------------------------------------------------------------------------------------------------------------------------
+
+// // Assignment 11,
+// //11. Guess game:  Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number. a. If user guesses the same number, show  “Bingo! Correct answer”.  b. If the guessed number +1 is the secret number, show  “Close enough to the correct answer”.   
+
+// var userNum = +prompt("Guess the Secert number");
+// var num = 4;
+// if (userNum === num) {
+//     document.write("<h1> “Bingo! Correct answer”.  </h1>");
+// }
+// else if (userNum === ++num) {
+//     document.write("Close enough to the correct answer");
+// }
+// else if (userNum === 3) {
+//     document.write("Close enough to the correct answer");
+// }
+// else {
+//     document.write("<h1>Wrong answer</h1>");
+// }
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 12,
+//12. Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.  
+
+// var num = prompt("Enter a number to check it is divisible by 3.");
+// var ans = "yes, " + num + " is divisible by 3.";
+// var aa = num % 3
+// if (aa === 0) {
+//     alert(ans);
+// }
+// else{
+//     alert(num + " cant divided by 3")
+// }
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// Assignment 13,
